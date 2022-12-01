631c45e353f9fb79f6b6269bde137dc167acabdb
GeniusOctopus <k.white221@outlook.com>
2022-12-01 11:06:59 +0100

    day1.

---
+++ Day1/Models/Elf.cs
@@ -0,0 +1,16 @@
+﻿namespace Day1.Models
+{
+    internal class Elf
+    {
+        public Elf() {}
+
+        public Elf(string name, List<int> carriedCalories)
+        {
+            Name = name;
+            CarriedCalories = carriedCalories;
+        }
+
+        public string Name { get; set; }
+        public List<int> CarriedCalories { get; set; }
+    }
+}
---
+++ Day1/Program.cs
@@ -0,0 +1,70 @@
+﻿using Day1.Models;
+using System.Text;
+
+namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var elfes = new List<Elf>();
+
+            // Part 1
+            try
+            {
+                using FileStream fs = new(Path.GetFullPath("../../../Input/Calories.txt"), FileMode.Open, FileAccess.Read);
+                using StreamReader reader = new(fs, Encoding.UTF8);
+
+                var elfCounter = 0;
+                elfes.Add(new Elf($"Elf {elfCounter + 1}", new List<int>()));
+                var line = reader.ReadLine();
+
+                while (line != null)
+                {
+                    if (line != "")
+                    {
+                        elfes[elfCounter].CarriedCalories.Add(int.Parse(line));
+                    }
+                    else
+                    {
+                        elfCounter++;
+                        elfes.Add(new Elf($"Elf {elfCounter + 1}", new List<int>()));
+                    }
+
+                    line = reader.ReadLine();
+                }
+
+                var maxCalories = elfes.Max(x => x.CarriedCalories.Sum());
+                var nameOfElf = elfes.First(x => x.CarriedCalories.Sum() == maxCalories).Name;
+
+                Console.WriteLine($"Elf count: {elfes.Count}");
+                Console.WriteLine($"Max calories: {maxCalories}");
+                Console.WriteLine($"Elf with most calories: {nameOfElf}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occured: {ex.Message}");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+            var sumTopThree = 0;
+
+            // Part 2
+            for (int i = 1; i < 4; i++)
+            {
+                var maxCalories = elfes.Max(x => x.CarriedCalories.Sum());
+                Console.WriteLine($"Max calories: {maxCalories}");
+                sumTopThree += maxCalories;
+                var indexOfTopElf = elfes.IndexOf(elfes.First(x => x.CarriedCalories.Sum() == maxCalories));
+                Console.WriteLine($"{i}. Elf: {elfes[indexOfTopElf].Name}");
+                elfes.RemoveAt(indexOfTopElf);
+            }
+
+            Console.WriteLine($"Sum of top three Elf's calories: {sumTopThree}");
+            Console.ReadLine();
+        }
+    }
+}
