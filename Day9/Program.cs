e7053b41d09956fc2e6367ec03f3e434153f6488
Kevin Weiss <kevin.weiss@scherdel.com>
2022-12-15 23:00:22 +0100

    refactoring.

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -4,15 +4,15 @@ namespace Day9
 {
     internal class Program
     {
-        static Point head1 = new(0, 0);
-        static Point tail1 = new(0, 0);
-        static List<Point> tailPositions1 = new() { tail1 };
-        static Point head2 = new(0, 0);
-        static List<Point> tail2 = new();
-        static List<Point> tailPositions2 = new();
-
         static void Main(string[] args)
         {
+            var head1 = new Point(0, 0);
+            var tail1 = new List<Point>() { new Point(0, 0) };
+            var tailPositions1 = new List<Point>() { new Point(0, 0) };
+            var head2 = new Point(0, 0);
+            var tail2 = new List<Point>();
+            var tailPositions2 = new List<Point>() { new Point(0, 0) };
+
             for (int i = 0; i < 9; i++)
                 tail2.Add(new Point(0, 0));
             tailPositions2.Add(tail2[8]);
@@ -26,8 +26,14 @@ namespace Day9
 
                 while (move is not null)
                 {
-                    Move1(move.First(), int.Parse(move[2..]));
-                    Move2(move.First(), int.Parse(move[2..]));
+                    var result1 = Move2(move.First(), int.Parse(move[2..]), head1, tail1, tailPositions1);
+                    head1 = result1.Item1;
+                    tail1 = result1.Item2;
+                    tailPositions1 = result1.Item3;
+                    var result2 = Move2(move.First(), int.Parse(move[2..]), head2, tail2, tailPositions2);
+                    head2 = result2.Item1;
+                    tail2 = result2.Item2;
+                    tailPositions2 = result2.Item3;
 
                     move = sr.ReadLine();
                 }
@@ -43,115 +49,69 @@ namespace Day9
             }
         }
 
-        static void Move1(char direction, int steps)
+        static (Point, List<Point>, List<Point>) Move2(char direction, int steps, Point head, List<Point> tail, List<Point> tailPositions)
         {
             for (int i = 0; i < steps; i++)
             {
                 switch (direction)
                 {
-                    case 'U': head1 = new Point(head1.X, head1.Y + 1); break;
-                    case 'L': head1 = new Point(head1.X - 1, head1.Y); break;
-                    case 'D': head1 = new Point(head1.X, head1.Y - 1); break;
-                    case 'R': head1 = new Point(head1.X + 1, head1.Y); break;
+                    case 'U': head = new Point(head.X, head.Y + 1); break;
+                    case 'L': head = new Point(head.X - 1, head.Y); break;
+                    case 'D': head = new Point(head.X, head.Y - 1); break;
+                    case 'R': head = new Point(head.X + 1, head.Y); break;
                     default: break;
                 }
 
-                if (Delta1())
-                {
-                    tail1 = GetPoint1(direction);
-                    if (!tailPositions1.Contains(tail1))
-                        tailPositions1.Add(tail1);
-                }
-            }
-        }
+                var headAux = head;
 
-        static void Move2(char direction, int steps)
-        {
-            for (int i = 0; i < steps; i++)
-            {
-                switch (direction)
+                for (int j = 0; j < tail.Count; j++)
                 {
-                    case 'U': head2 = new Point(head2.X, head2.Y + 1); break;
-                    case 'L': head2 = new Point(head2.X - 1, head2.Y); break;
-                    case 'D': head2 = new Point(head2.X, head2.Y - 1); break;
-                    case 'R': head2 = new Point(head2.X + 1, head2.Y); break;
-                    default: break;
-                }
-
-                var headAux = head2;
-
-                for (int j = 0; j < tail2.Count; j++)
-                {
-                    if (Delta2(j, headAux))
+                    if (Delta2(headAux, tail[j]))
                     {
-                        tail2[j] = GetPoint2(headAux, j);
-                        if (!tailPositions2.Contains(tail2[^1]) && j == tail2.Count - 1)
-                            tailPositions2.Add(tail2[^1]);
+                        tail[j] = GetPoint2(headAux, tail[j]);
+                        if (!tailPositions.Contains(tail[^1]) && j == tail.Count - 1)
+                            tailPositions.Add(tail[^1]);
                     }
-                    headAux = tail2[j];
+                    headAux = tail[j];
                 }
             }
+
+            return (head, tail, tailPositions);
         }
 
-        static bool Delta1()
+        static bool Delta2(Point head, Point tail)
         {
-            return (Math.Abs(head1.X - tail1.X) > 1 || Math.Abs(head1.Y - tail1.Y) > 1);
+            return (Math.Abs(head.X - tail.X) > 1 || Math.Abs(head.Y - tail.Y) > 1);
         }
 
-        static bool Delta2(int i, Point head)
-        {
-            return (Math.Abs(head.X - tail2[i].X) > 1 || Math.Abs(head.Y - tail2[i].Y) > 1);
-        }
-
-        static Point GetPoint1(char direcion)
-        {
-            return direcion switch
-            {
-                'U' => new Point(head1.X, head1.Y - 1),
-                'L' => new Point(head1.X + 1, head1.Y),
-                'D' => new Point(head1.X, head1.Y + 1),
-                'R' => new Point(head1.X - 1, head1.Y),
-                _ => new Point(head1.X, head1.Y),
-            };
-        }
-
-        static Point GetPoint2(Point head, int j)
+        static Point GetPoint2(Point head, Point tail)
         {
             // rechts
-            if (head.X - tail2[j].X >= 1 && head.Y - tail2[j].Y == 0)
-                return new Point(tail2[j].X + 1, tail2[j].Y);
+            if (head.X - tail.X >= 1 && head.Y - tail.Y == 0)
+                return new Point(tail.X + 1, tail.Y);
             // links
-            if (head.X - tail2[j].X <= -1 && head.Y - tail2[j].Y == 0)
-                return new Point(tail2[j].X - 1, tail2[j].Y);
+            if (head.X - tail.X <= -1 && head.Y - tail.Y == 0)
+                return new Point(tail.X - 1, tail.Y);
             // oben
-            if (head.X - tail2[j].X == 0 && head.Y - tail2[j].Y >= 1)
-                return new Point(tail2[j].X, tail2[j].Y + 1);
+            if (head.X - tail.X == 0 && head.Y - tail.Y >= 1)
+                return new Point(tail.X, tail.Y + 1);
             // unten
-            if (head.X - tail2[j].X == 0 && head.Y - tail2[j].Y <= -1)
-                return new Point(tail2[j].X, tail2[j].Y - 1);
+            if (head.X - tail.X == 0 && head.Y - tail.Y <= -1)
+                return new Point(tail.X, tail.Y - 1);
             // oben rechts
-            if (head.X - tail2[j].X >= 1 && head.Y - tail2[j].Y >= 1)
-                return new Point(tail2[j].X + 1, tail2[j].Y + 1);
+            if (head.X - tail.X >= 1 && head.Y - tail.Y >= 1)
+                return new Point(tail.X + 1, tail.Y + 1);
             // oben links
-            if (head.X - tail2[j].X <= -1 && head.Y - tail2[j].Y >= 1)
-                return new Point(tail2[j].X - 1, tail2[j].Y + 1);
+            if (head.X - tail.X <= -1 && head.Y - tail.Y >= 1)
+                return new Point(tail.X - 1, tail.Y + 1);
             // unten rechts
-            if (head.X - tail2[j].X >= 1 && head.Y - tail2[j].Y <= -1)
-                return new Point(tail2[j].X + 1, tail2[j].Y - 1);
+            if (head.X - tail.X >= 1 && head.Y - tail.Y <= -1)
+                return new Point(tail.X + 1, tail.Y - 1);
             // unten links
-            if (head.X - tail2[j].X <= -1 && head.Y - tail2[j].Y <= -1)
-                return new Point(tail2[j].X - 1, tail2[j].Y - 1);
+            if (head.X - tail.X <= -1 && head.Y - tail.Y <= -1)
+                return new Point(tail.X - 1, tail.Y - 1);
 
-
-            return tail2[j];
-            //return direcion switch
-            //{
-            //    'U' => new Point(head2.X, head2.Y - 1),
-            //    'L' => new Point(head2.X + 1, head2.Y),
-            //    'D' => new Point(head2.X, head2.Y + 1),
-            //    'R' => new Point(head2.X - 1, head2.Y),
-            //    _ => new Point(head2.X, head2.Y),
-            //};
+            return tail;
         }
     }
 }
